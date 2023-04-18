d94a4ea984c9bc7dfc64d8fef7083f8195dcfe58
Garet Halliday <me@garet.holiday>
2023-04-18 17:03:11 -0500

    benchmarks

--- lib/util/race/send_test.go
+++ lib/util/race/send_test.go
@@ -1,6 +1,7 @@
 package race
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -16,8 +17,77 @@ func TestSend(t *testing.T) {
 			}
 			return chans[i], true
 		}, 1)
+		Send(func(i int) (chan<- int, bool) {
+			if i >= len(chans) {
+				return nil, false
+			}
+			return chans[i], true
+		}, 2)
+		Send(func(i int) (chan<- int, bool) {
+			if i >= len(chans) {
+				return nil, false
+			}
+			return chans[i], true
+		}, 3)
 	}()
 	if <-chans[7] != 1 {
 		panic("expected to receive 1")
+	}
+	if <-chans[3] != 2 {
+		panic("expected to receive 2")
+	}
+	if <-chans[5] != 3 {
+		panic("expected to receive 3")
+	}
+}
+
+func BenchmarkSend(b *testing.B) {
+	var chans []chan int
+	for i := 0; i < 10; i++ {
+		chans = append(chans, make(chan int))
+	}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v := rand.Int()
+		go func() {
+			if <-chans[rand.Intn(10)] != v {
+				panic("expected correct value")
+			}
+		}()
+		Send(func(i int) (chan<- int, bool) {
+			if i >= len(chans) {
+				return nil, false
+			}
+			return chans[i], true
+		}, v)
+	}
+}
+
+func BenchmarkSend_Raw(b *testing.B) {
+	var chans []chan int
+	for i := 0; i < 10; i++ {
+		chans = append(chans, make(chan int))
+	}
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v := rand.Int()
+		go func() {
+			if <-chans[rand.Intn(10)] != v {
+				panic("expected correct value")
+			}
+		}()
+		// Send above is equal to the following
+		select {
+		case chans[0] <- v:
+		case chans[1] <- v:
+		case chans[2] <- v:
+		case chans[3] <- v:
+		case chans[4] <- v:
+		case chans[5] <- v:
+		case chans[6] <- v:
+		case chans[7] <- v:
+		case chans[8] <- v:
+		case chans[9] <- v:
+		}
 	}
 }
