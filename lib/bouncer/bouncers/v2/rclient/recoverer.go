52e810309c329e54ecc2df825552cde008ebee33
Garet Halliday <me@garet.holiday>
2023-05-19 19:37:13 -0500

    thinking

--- lib/bouncer/bouncers/v2/rclient/recoverer.go
+++ lib/bouncer/bouncers/v2/rclient/recoverer.go
@@ -3,6 +3,32 @@ package rclient
 import "pggat2/lib/bouncer/bouncers/v2/bctx"
 
 func Recover(ctx *bctx.Context) error {
-	// TODO(garet) actually recover client
+	// TODO(garet) I actually don't know if I have to obey the client's expectations when it comes to Txs
+	// We are just going to break out without letting the client tell us to. This might lead to app crashes,
+	// but if a database is crashing or behaving badly, you can't really expect any better behavior
+	if inCopyIn {
+		// TODO(garet) wait until client sends CopyDone or CopyFail
+	}
+	if inCopyOut {
+		// TODO(garet) send ErrorResponse("server failed")
+	}
+	if inQuery {
+		// TODO(garet) send ErrorResponse("server failed")
+		// TODO(garet) send ReadyForQuery('I')
+	}
+	if inFunctionCall {
+		// TODO(garet) send ErrorResponse("server failed")
+		// TODO(garet) send ReadyForQuery('I')
+	}
+	if inSync {
+		// TODO(garet) send ErrorResponse("server failed")
+		// TODO(garet) send ReadyForQuery('I')
+	}
+	if inEQP {
+		// TODO(garet) discard until Sync and then send error and ReadyForQuery('I')
+	}
+	if ctx.TxState != 'I' {
+		// TODO(garet) wait for next packet and then handle it, sending error and ReadyForQuery('I')
+	}
 	return nil
 }
--- lib/bouncer/bouncers/v2/rserver/recoverer.go
+++ lib/bouncer/bouncers/v2/rserver/recoverer.go
@@ -5,6 +5,26 @@ import (
 )
 
 func Recover(ctx *bctx.Context) error {
-	// TODO(garet) actually recover server
+	if inCopyOut {
+		// TODO(garet) wait for CopyDone or ErrorResponse
+	}
+	if inCopyIn {
+		// TODO(garet) send CopyFail
+	}
+	if inQuery {
+		// TODO(garet) wait for ready for query, waiting for copyOut if it happens, failing copyIn if it happens
+	}
+	if inFunctionCall {
+		// TODO(garet) wait for ready for query
+	}
+	if inSync {
+		// TODO(garet) wait for ready for query
+	}
+	if inEQP {
+		// TODO(garet) send sync and wait for ready for query
+	}
+	if ctx.TxState != 'I' {
+		// TODO(garet) send Query('ABORT;') and wait for ReadyForQuery
+	}
 	return nil
 }
