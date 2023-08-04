dca69471fe8edeb12806ae74ad4f2d65c62c69e1
Garet Halliday <me@garet.holiday>
2023-08-04 17:39:32 -0500

    a

--- lib/util/ini/unmarshal.go
+++ lib/util/ini/unmarshal.go
@@ -79,7 +79,7 @@ outer:
 			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 			reflect.Float32, reflect.Float64,
-			reflect.Slice:
+			reflect.Array, reflect.Slice:
 			break outer
 		default:
 			return errors.New("cannot set value of this type")
