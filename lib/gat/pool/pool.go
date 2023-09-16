9a0d6c74a50508f1e4413df4d2bb15efa05aa50e
Garet Halliday <me@garet.holiday>
2023-09-16 04:44:57 -0500

    delete instead of release

--- lib/gat/pool/pool.go
+++ lib/gat/pool/pool.go
@@ -273,7 +273,7 @@ func (T *Pool) acquireServer(client *Client) *Server {
 		server, ok := T.servers[serverID]
 		T.mu.RUnlock()
 		if !ok {
-			T.options.Pooler.Release(serverID)
+			T.options.Pooler.DeleteServer(serverID)
 			continue
 		}
 		return server
