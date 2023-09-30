d985ace48b0eab64ce8747df8fdf2bf19a56325c
Garet Halliday <me@garet.holiday>
2023-09-29 19:30:54 -0500

    ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 EXEC_BIN_PATH=${PGGAT_BIN_PATH:=/usr/bin/pggat}
 
 pggat() {
-    exec $EXEC_BIN_PATH ${@}
+    if [[ ! -v "${PGGAT_RUN_MODE}" ]]; then
+      exec $EXEC_BIN_PATH run
+    else
+      exec $EXEC_BIN_PATH run --adapter="caddyfile" --config="/presets/${PGGAT_RUN_MODE}.Caddyfile"
+    fi
 }
-
-
-case "${1}" in
-    "")
-        pggat ${@}
-        ;;
-    *)
-        export PGGAT_RUN_MODE=${1}
-        shift
-        pggat ${@}
-        ;;
-esac
