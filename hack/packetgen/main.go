363c19e6efc53e445bc4c4a27c717c6526b04f64
Garet Halliday <me@garet.holiday>
2023-10-10 18:14:44 -0500

    just need to fix Authentication packet

--- hack/packetgen/main.go
+++ hack/packetgen/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 
 	"gfx.cafe/util/temple"
 	"gfx.cafe/util/temple/lib/prayer"
 )
 
 func main() {
+	var idx int
 	var obj any
 	temple.RegisterTemplateDir("templates")
 	temple.ReadObjectFile(&obj, "protocol.yaml")
+	temple.RegisterFunc("temp", func() string {
+		idx++
+		return fmt.Sprintf("temp%d", idx)
+	})
 	temple.Prepare(&prayer.Go{
 		Input:   "packets",
 		Obj:     obj,
