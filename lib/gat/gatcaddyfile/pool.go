58487ba505040a28a394e1fce13189ab889c6fba
Garet Halliday <me@garet.holiday>
2024-05-08 22:06:32 -0500

    make not default

--- lib/gat/gatcaddyfile/pool.go
+++ lib/gat/gatcaddyfile/pool.go
@@ -23,8 +23,6 @@ func defaultTrackedParameters() []strutil.CIString {
 		strutil.MakeCIString("timezone"),
 		strutil.MakeCIString("standard_conforming_strings"),
 		strutil.MakeCIString("application_name"),
-		strutil.MakeCIString("intervalstyle"),
-		strutil.MakeCIString("search_path"),
 	}
 }
 
